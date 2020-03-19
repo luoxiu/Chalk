0aa9d59b6b5f05adabb402d749931a6e1076bc75
luoxiu <luoxiustm@gmail.com>
2020-03-19 18:21:43 +0800

    rename q to chalk-app

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ let package = Package(
     name: "Chalk",
     products: [
         .library(name: "Chalk", targets: ["Chalk"]),
-        .executable(name: "Q", targets: ["Q"]),
+        .executable(name: "chalk-app", targets: ["chalk-app"]),
     ],
     dependencies: [
         .package(url: "https://github.com/luoxiu/Rainbow", from: "0.0.1"),
     ],
     targets: [
         .target(name: "Chalk", dependencies: ["Rainbow"]),
-        .target(name: "Q", dependencies: ["Chalk"]),
+        .target(name: "chalk-app", dependencies: ["Chalk"]),
         .testTarget(name: "ChalkTests", dependencies: ["Chalk"]),
     ]
 )
---
+++ Sources/chalk-app/main.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by jinxiangqiu on 15/3/2020.
+//
+
+import Foundation
+import Chalk
+
+let name = """
+  ______  __    __       ___       __       __  ___
+ /      ||  |  |  |     /   \\     |  |     |  |/  /
+|  ,----'|  |__|  |    /  ^  \\    |  |     |  '  /
+|  |     |   __   |   /  /_\\  \\   |  |     |    <
+|  `----.|  |  |  |  /  _____  \\  |  `----.|  .  \\
+ \\______||__|  |__| /__/     \\__\\ |_______||__|\\__\\
+"""
+
+print(
+    name.map {
+            ck.fg(.random).on(String($0))
+        }
+        .reduce(TerminalString()) {
+            $0 + $1
+        }
+)
